e7e89ce2a4d2290347f434344584b0769da721f8
cmzluna <claudiomazzoli@gmail.com>
2021-06-07 15:05:08 -0300

    added event listeners

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const gameBoard = (function () {
-  let gameboard = [];
+  let board = ["X", "O", "X", "X", "X", "X", "O", "X", "O"];
 
+  // gameboard object?
   return {
-    gameboard,
+    board,
   };
 })();
 
-const displayController = (function () {
-  const renderBoardArray = () => {
-    console.log(gameBoard.gameboard);
-  };
-})();
 //
 const players = (name) => {
   const sayHello = () => console.log("hello!");
 
   return { name, sayHello }; // name is same as name:name
 };
+
+const displayController = (function () {
+  const values = document.querySelectorAll(".box");
+  const restartBtn = document.getElementById("restart-btn");
+
+  // add event listeners of boxes
+  document.querySelector("#board").addEventListener("click", (e) => {
+    // event delegation
+    const boxClicked = e.target; // get ID of clicked box
+    console.log(boxClicked["id"]);
+  });
+  // don't do anything if gameIsOver or if clicked on same box
+  // listeners should have update board
+
+  restartButton.addEventListener("click", (e) => {
+    // reset board
+
+    // reset players
+
+    const renderBoard = () => {
+      // render values from board
+    };
+
+    // Result message
+
+    // Turn message
+  })();
+})();
+
+// const gameController = (function () => {
+
+//   //
+// })();
